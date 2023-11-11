2d874dcadee1058929901490c1a915fc9a0afb20
jmgvi <jmgvizuete@gmail.com>
2023-11-11 16:35:15 +0100

    DES-22  Diseño gráfico - Prototipo

--- src/test/java/com/udima/tfg/tfg/TfgApplicationTests.java
+++ src/test/java/com/udima/tfg/tfg/TfgApplicationTests.java
@@ -1,6 +1,6 @@
 package com.udima.tfg.tfg;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
